5175cb2c76661560e9109fb737b3b6601cac2b3a
VivekSreenivasan <vivek.sreenivasan5@gmail.com>
2017-07-27 21:20:13 -0400

    edits

--- xml.php
+++ xml.php
@@ -1,9 +1,23 @@
 <?php
     $xml = new DOMDocument("1.0","UTF-8");
+    $xml->formatOutput=true;
     /*create element using createElement()
     append child to parent using appendChild()*/
 
-    $ticker=$xml->createElement("ticker");
-    $xml->appendChild($ticker);
+    $tickers=$xml->createElement("tickers");
+    $xml->appendChild($tickers);
 
+    $ticker = $xml->createElement("ticker");
+    $ticker->setAttribute("id",1);
+    $tickers->appendChild($ticker);
+
+    $name=$xml->createElement("symbol","GME");
+    $ticker->appendChild($name);
+
+    $link=$xml->createElement("link","www.google.com");
+    $ticker->appendChild($link);
+
+    echo "<xmp>".$xml->saveXML()."</xmp>";
+
+    $xml->save("tickers.xml") or die("Error,Unable to create XML file");
 ?>
